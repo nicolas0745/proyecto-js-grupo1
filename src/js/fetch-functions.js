b62a77430c6b6305639dd31e44d8a6ccd9b3d953
nicolas0745 <133726349+nicolas0745@users.noreply.github.com>
2023-12-07 22:36:33 -0500

    agrego 2 funciones fetchBySearch, fetchById

--- src/js/fetch-functions.js
+++ src/js/fetch-functions.js
@@ -1,13 +1,13 @@
 const axios = require('axios').default;
-const urlTrending = 'https://api.themoviedb.org/3/trending/movie/day';
 axios.defaults.headers.common['Authorization'] =
   'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MzlhNjJlYTE5ZmQzZmUwYjhhZGE5NTM4Nzc1ZTZmOSIsInN1YiI6IjY1NzBjOWUwYTc2YWM1MDEzOTA5NzllNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2xkJugkWffi98YAAJOAs6dLpuODWV6jhron_0UeVaJg';
 
-// Function to select trendig movies of the day.
+//////////////// Function to select trendig movies of the day.  ///////////////////////////
 
 export async function getTrending(page) {
+  const url = 'https://api.themoviedb.org/3/trending/movie/day';
   try {
-    const response = await axios.get(urlTrending, {
+    const response = await axios.get(url, {
       params: {
         language: 'en-US',
         page,
@@ -21,6 +21,49 @@ export async function getTrending(page) {
     return data;
   } catch (error) {
     console.error(error);
-    return error;
+  }
+}
+
+//////////           Function to search a movie with a string       ///////
+
+export async function fetchBySearch(query, page) {
+  const url = 'https://api.themoviedb.org/3/search/movie';
+  try {
+    const response = await axios.get(url, {
+      params: {
+        query,
+        language: 'en-US',
+        page,
+      },
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    if (!response.statusText == 'OK') throw new Error('Response Failed');
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//////////// function to fetch information of an especific movie //////////////////////
+
+export async function fetchById(id) {
+  const url = `https://api.themoviedb.org/3/movie/${id}`;
+  try {
+    const response = await axios.get(url, {
+      params: {
+        language: 'en-US',
+      },
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    if (!response.statusText == 'OK') throw new Error('Response Failed');
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error(error);
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,28 @@
-import { getTrending } from './fetch-functions';
+import { getTrending, fetchBySearch, fetchById } from './fetch-functions';
 let page = 1;
+let pageSearch = 1;
 
-const getMovies = async () => {
+const getTrendingMovies = async () => {
   const data = await getTrending(page);
+  if (data == undefined) return;
   const { results: movies, total_pages } = data;
   console.log(movies);
   console.log(total_pages);
 };
 
-getMovies();
+const searchMovies = async query => {
+  const data = await fetchBySearch(query, pageSearch);
+  if (data == undefined) return;
+  const { results: movies, total_pages } = data;
+  console.log(movies);
+  console.log(total_pages);
+};
+
+const fetchMovieById = async id => {
+  const data = await fetchById(id);
+  console.log(data);
+};
+
+getTrendingMovies();
+searchMovies('mario');
+fetchMovieById(1209403);
