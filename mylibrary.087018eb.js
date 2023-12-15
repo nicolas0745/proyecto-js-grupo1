4645ec0204915ff31af2214a105a8f8b126f1f4c
nicolas0745 <133726349+nicolas0745@users.noreply.github.com>
2023-12-15 04:52:06 +0000

    Deploying to gh-pages from @ nicolas0745/proyecto-js-grupo1@762708c3fbd36b13e653448ba3d7ab079db82187 🚀

--- mylibrary.087018eb.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".btn-watched"),t=document.querySelector(".btn-queue");t.addEventListener("click",(n=>{t.style.background="rgba(255, 107, 8, 1)",e.style.background="transparent"})),e.addEventListener("click",(n=>{e.style.background="rgba(255, 107, 8, 1)",t.style.background="transparent"}));
-//# sourceMappingURL=mylibrary.087018eb.js.map
--- mylibrary.184c560c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".btn-watched"),n=document.querySelector(".btn-queue");n.addEventListener("click",(function(t){n.style.background="rgba(255, 107, 8, 1)",e.style.background="transparent"})),e.addEventListener("click",(function(t){e.style.background="rgba(255, 107, 8, 1)",n.style.background="transparent"}))}();
-//# sourceMappingURL=mylibrary.184c560c.js.map
